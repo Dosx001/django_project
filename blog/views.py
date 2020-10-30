4bb056724271b0cc7b1d947bbe15add20f239c21
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-10-29 19:56:15 -0700

    created posts for blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'Andres',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 29, 2020' 
+    },
+    {
+        'author' : 'Andres',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 29, 2020'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
