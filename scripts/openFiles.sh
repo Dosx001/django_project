dbbc57b7371db8adaaa6977a5bbcc3c11cd9adc5
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-11-05 19:53:51 -0800

    Updated script

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-vim -p blog/views.py blog/templates/blog/about.html blog/templates/blog/base.html \
-    "+tabn | vs blog/templates/blog/home.html"
+vim -p blog/views.py django_project/urls.py blog/templates/blog/base.html \
+    django_project/settings.py users/views.py \
+#!    users/templates/users/dd
+#! "+tabn | vs blog/templates/blog/home.html"
